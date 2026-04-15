6aff06dd6b1d1894ca81c42d21817fb48ce56d3c
Dante <dantedelorenzo@hotmail.com>
2026-04-15 20:06:36 -0300

    Ejercicio 1: variables, template literals y contador

---
+++ Ejercicios/c03-javascript/app.js
@@ -0,0 +1,13 @@
+const nombre = "Dante";
+const edad = 21;
+const materia = "Desarrollo de Software";
+
+console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años y estoy cursando la materia ${materia}.`);
+
+let contador = 0;
+
+for (let i = 0; i < 3; i++) {
+    contador++;
+}
+
+console.log(`El contador tiene el valor: ${contador}`);

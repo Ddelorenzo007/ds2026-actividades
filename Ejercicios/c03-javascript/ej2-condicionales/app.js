6d29762cfbbab84fd5300953d200bc29a518dad2
Dante <dantedelorenzo@hotmail.com>
2026-04-15 20:52:07 -0300

    Ejercicio 2: funciones con condicionales y switch

---
+++ Ejercicios/c03-javascript/ej2-condicionales/app.js
@@ -0,0 +1,30 @@
+function clasificarNota(nota){
+    if (nota < 4){
+        return "Desaprobado";
+    } else if (nota >= 4 && nota <= 7){
+        return "Aprobado";
+    } else if (nota >= 8 && nota <= 10){
+        return "Promocionado";
+    }
+}
+
+function diaDeLaSemana(numero){
+    switch(numero){
+        case 1:
+            return "Lunes";
+        case 2:
+            return "Martes";
+        case 3:
+            return "Miércoles";
+        case 4:
+            return "Jueves";
+        case 5:
+            return "Viernes";
+        case 6:
+            return "Sábado (fin de semana)";
+        case 7:
+            return "Domingo (fin de semana)";
+        default:
+            return "Día invalido";
+    }
+}

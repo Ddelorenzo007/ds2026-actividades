4729400f07a6dbe210b5b3929c3a5970b8afada9
Dante <dantedelorenzo@hotmail.com>
2026-07-28 16:00:04 -0300

    C14

---
+++ Ejercicios/c14-backend/backend/src/data/libros.ts
@@ -0,0 +1,25 @@
+export const libros = [
+    {    "id": 1,
+      "titulo": "El Principito",
+      "autor": "Antoine de Saint-Exupéry",
+      "precio": 12500,
+      "imagen": "https://images.cdn1.buscalibre.com/fit-in/360x360/75/23/75237f2f609042d9713cb03b681c77c1.jpg",
+      "disponible": true
+    },
+    {
+        "id": 2,
+      "titulo": "1984",
+      "autor": "George Orwell",
+      "precio": 15000,
+      "imagen": "https://covers.openlibrary.org/b/isbn/9788499890944-L.jpg",
+      "disponible": false
+    },
+    {
+        "id": 3,
+      "titulo": "Rayuela",
+      "autor": "Julio Cortázar",
+      "precio": 18000,
+      "imagen": "https://upload.wikimedia.org/wikipedia/commons/c/ca/Rayuela_JC.png",
+      "disponible": true
+    }
+]

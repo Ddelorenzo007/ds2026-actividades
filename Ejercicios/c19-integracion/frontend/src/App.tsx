1678ad0687b184f0075eeace64bd28b811365add
Dante <dantedelorenzo@hotmail.com>
2026-09-12 18:28:48 -0300

    Paso 5

--- Ejercicios/c19-integracion/frontend/src/App.tsx
+++ Ejercicios/c19-integracion/frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 import Libros from './pages/Catalogo';
+import Login from './pages/Login';
+import LibroNuevo from './pages/LibroNuevo';
 import { Routes, Route } from 'react-router-dom';
 import { BusquedaProvider } from './context/BusquedaContext'; 
 
@@ -14,6 +16,8 @@ function App() {
           <Route path="/catalogo" 
             element={<Libros />}
           />
+          <Route path="/login" element={<Login />} />
+          <Route path="/libros/nuevo" element={<LibroNuevo />} /> 
         </Routes>
       </Layout>
     </BusquedaProvider>
--- Ejercicios/c19-integracion/frontend/src/components/Layout/Navbar.tsx
+++ Ejercicios/c19-integracion/frontend/src/components/Layout/Navbar.tsx
@@ -1,8 +1,18 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from 'react-router-dom';                           
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { obtenerToken, borrarToken } from '../../services/sesion';
 
 function NavBar() {
+    const navigate = useNavigate();  
+    const estaLogueado = !!obtenerToken();
+  const manejarSesion = () => {
+    if (estaLogueado) {
+      borrarToken();
+      navigate('/');
+    } else {
+      navigate('/login');
+    }
+  };
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -12,6 +22,9 @@ function NavBar() {
           <Nav className="me-auto">
             <Nav.Link href="#">Contacto</Nav.Link>
             <Nav.Link href="/catalogo">Catálogo</Nav.Link>
+            <button className="btn-login ms-lg-3 mt-2 mt-lg-0" onClick={manejarSesion}>
+              {estaLogueado ? 'Salir' : 'Ingresar'}
+            </button>
           </Nav>
         </Navbar.Collapse>
       </Container>
